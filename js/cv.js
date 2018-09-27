d12570ed88406124aee3f9d6e3d72a0b9baac47a
yassinehamzaoui <yassinexhamzaoui@gmail.com>
2018-09-27 07:51:31 +0200

    12 eme commit

--- js/cv.js
+++ js/cv.js
@@ -40,3 +40,22 @@ function SizeProgress(multiple) {
 	// after 2 seconds stop
 	setTimeout(() => { clearInterval(timerId);}, 1000);
 }
+
+function tooltip_move(e) {
+  var tooltip = e.target.classList.contains("coupontooltip")
+      ? e.target
+      : e.target.querySelector(":scope .coupontooltip");
+  tooltip.style.left =
+      (e.pageX + tooltip.clientWidth + 10 < document.body.clientWidth)
+          ? (e.pageX + 10 + "px")
+          : (document.body.clientWidth + 5 - tooltip.clientWidth + "px");
+  tooltip.style.top =
+      (e.pageY + tooltip.clientHeight + 10 < document.body.clientHeight)
+          ? (e.pageY + 10 + "px")
+          : (document.body.clientHeight + 5 - tooltip.clientHeight + "px");
+}
+
+var tooltips = document.querySelectorAll('.couponcode');
+for(var i = 0; i < tooltips.length; i++) {
+  tooltips[i].addEventListener('mousemove', showTooltip);
+}
